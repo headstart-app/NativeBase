0cc7016321c4765d27d796712a8091a76987614c
Mentioum <mentioummetis@gmail.com>
2016-09-13 01:27:42 +0100

    set content inset to 0

--- Components/Widgets/Content.js
+++ Components/Widgets/Content.js
@@ -32,7 +32,7 @@ export default class Content extends NativeBaseComponent {
     const contentContainerStyle = this.props.contentContainerStyle || {};
     contentContainerStyle.padding = (this.props.padder) ? this.getTheme().contentPadding : 0;
     return(
-      <KeyboardAwareScrollView ref={c => this._scrollview = c} resetScrollToCoords={{x:0,y:0}} {...this.prepareRootProps()} contentContainerStyle={contentContainerStyle}>{this.props.children}</KeyboardAwareScrollView>
+      <KeyboardAwareScrollView ref={c => this._scrollview = c} resetScrollToCoords={{x:0,y:0}} {...this.prepareRootProps()} contentContainerStyle={contentContainerStyle} contentInset={{bottom:0}}>{this.props.children}</KeyboardAwareScrollView>
     );
   }
 }
